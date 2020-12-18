3e08440ea2379a9691c8f386917203b5b2cf23ae
Andreas Timoudas <andreas@Andreass-Air.lan>
2020-12-18 23:25:22 +0100

    getplayers and getteams

--- services/TeamPlayersService.js
+++ services/TeamPlayersService.js
@@ -1,18 +1,44 @@
 const { TeamSquadsModel } = require("../models/TeamSquads");
 
 async function getTeams(seasonId){
-    var data = await ScheduleModel.aggregate()
-    .sort({
-        'provisionalKickoff.millis': -1
+    var data = await TeamSquadsModel.aggregate()
+    .match({
+        'seasonId': parseInt(seasonId)
+    })
+    .group({
+        '_id':{
+        'teamId': '$teamId',
+        'teamName': '$teamName',
+        'teamAbbr': '$teamAbbr'
+        } 
     })
     .project({
-        'kickoffLabel': {
-            '$substr': ['$provisionalKickoff.label', 17, 5]
-        },
-        'teams': 1,
-
-
+        'teamId': '$_id.teamId',
+        'teamName': '$_id.teamName',
+        '_id': 0
     })
-    .limit(10)
+    .sort({
+        'teamName': 1
+    })
     console.log(data)
+    return data
 }
+
+async function getPlayers(seasonId, teamId){
+    var data = await TeamSquadsModel.aggregate()
+    .match({
+        'seasonId': parseInt(seasonId),
+        'teamId': parseInt(teamId)
+    })
+
+    .project({
+        'players': 1
+    })
+    .unwind(
+        'players'
+    )
+
+    console.log(data)
+    return data
+}
+getPlayers(274, 1)
